34e5e854e956c8b15668a17a88f85c8f0aa943ab
Douglas Prado <douglasrafaeldoprado@gmail.com>
2025-11-11 09:21:00 -0300

    Add LangGraph instrumentation and fallback monitoring

--- agents/utils/__init__.py
+++ agents/utils/__init__.py
@@ -1,1 +1,9 @@
 """Utilitários compartilhados pelos agentes."""
+
+from .instrumentation import GraphCallbackHandler, MetricsCollector, ProcessGraphRunner
+
+__all__ = [
+    "GraphCallbackHandler",
+    "MetricsCollector",
+    "ProcessGraphRunner",
+]
